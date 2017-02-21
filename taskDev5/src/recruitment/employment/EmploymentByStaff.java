37a9865ff0ee756d956c9a9ce2cd0518b40e5011
Pavel Yachkurynski <yachkupa@gmail.com>
2017-02-22 01:21:19 +0300

    change one of ways of employment

--- taskDev5/src/recruitment/employment/EmploymentByStaff.java
+++ taskDev5/src/recruitment/employment/EmploymentByStaff.java
@@ -8,7 +8,7 @@ import recruitment.employees.*;
  */
 public class EmploymentByStaff extends Employment {
   private static final String ENTER_VALUE_MSG = "Enter productivity: ";
-  private static final String UNUSELESS_QUALIFICATION = "Junior";
+  private static final String USELESS_QUALIFICATION = "Junior";
 
   /**
    * Performs employment with minimal number of employees higher than Junior for fixed productivity.
@@ -19,24 +19,23 @@ public class EmploymentByStaff extends Employment {
    */
   public List<Employee> employ(int productivity, List<Employee> employees) {
     List<Employee> team = new ArrayList<>();
-    List<Employee> bufferedTeam = new ArrayList<>();
-
-    for (Employee employee : employees) {
-      if (employee.getQualification().equals(UNUSELESS_QUALIFICATION)) {
-        continue;
-      } else {
-        bufferedTeam.add(employee);
-      }
-    }
+    List<Employee> bufferedTeam = getListWithoutOneQualific(USELESS_QUALIFICATION, employees);
 
     for (Employee employee : bufferedTeam) {
       while (productivity - employee.getProductivity() >= 0) {
         team.add(employee);
         productivity -= employee.getProductivity();
-      }
 
-      if(bufferedTeam.indexOf(employee) == bufferedTeam.size() - 1 && productivity > 0) {
-        team.add(employee);
+        if (bufferedTeam.indexOf(employee) == 0) {
+          Employee secondQualific = bufferedTeam.get(1);
+          Employee thirdQualific = bufferedTeam.get(2);
+
+          if (secondQualific.getProductivity()+ thirdQualific.getProductivity() == productivity) {
+            team.add(secondQualific);
+            team.add(thirdQualific);
+            productivity -= (secondQualific.getProductivity()+ thirdQualific.getProductivity());
+          }
+        }
       }
     }
     return team;
@@ -49,5 +48,25 @@ public class EmploymentByStaff extends Employment {
    */
   public String getMessage() {
     return ENTER_VALUE_MSG;
+  }
+
+  /**
+   * Returns list without one useless qualification.
+   *
+   * @param qualification - useless qualification
+   * @param employees - list of all qualifications.
+   * @return list without useless qualification.
+   */
+  public List<Employee> getListWithoutOneQualific(String qualification, List<Employee> employees) {
+    List<Employee> bufferedTeam = new ArrayList<>();
+
+    for (Employee employee : employees) {
+      if (employee.getQualification().equals(qualification)) {
+        continue;
+      } else {
+        bufferedTeam.add(employee);
+      }
+    }
+    return bufferedTeam;
   }
 }
